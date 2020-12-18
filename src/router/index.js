e176c01d5a60062fe038c409df320cf2e181c492
taihuxiaoxin <ta198410zsy@163.com>
2020-12-18 08:52:55 +0800

    更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
+import filmrouter from "@/router/routers/films"
+import cinrouter from "@/router/routers/cinms"
+import cenrouter from "@/router/routers/center"
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  filmrouter,
+  cinrouter,
+  cenrouter,
+  {path:'/',redirect: "/film"}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
